8802b38d21704058cf13f193d0849f9782b8bf41
Chris Mann <chrispmann@gmail.com>
2024-02-22 15:14:00 -0800

    added streaming to aanaphi2.cpu

--- aanaphi2-cpu.py
+++ aanaphi2-cpu.py
@@ -1,5 +1,6 @@
-import transformers
 import torch
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 
 compute_dtype = torch.float32
 cache_path    = ''
@@ -17,12 +18,15 @@ def prompt_format(prompt):
     return out
 model.eval()
 
+streamer = TextStreamer(tokenizer)
+
 @torch.no_grad()
 def generate(prompt, max_length=500):
     prompt_chat = prompt_format(prompt)
     inputs      = tokenizer(prompt_chat, return_tensors="pt", return_attention_mask=True).to('cpu')
-    outputs     = model.generate(**inputs, max_length=max_length, eos_token_id= tokenizer.eos_token_id) 
+    outputs     = model.generate(**inputs, max_length=max_length, streamer=streamer,  eos_token_id= tokenizer.eos_token_id) 
     text        = tokenizer.batch_decode(outputs[:,:-1])[0]
+    
     return text
 
 while True:
